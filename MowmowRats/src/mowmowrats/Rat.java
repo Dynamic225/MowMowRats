c198054c17521b50bd042f70e195276e1812bdac
Anthony <anthonywitherspoon225@gmail.com>
2022-10-22 16:35:39 -0500

    Added implemented Board Class Prepared classes Player and Rat to be defined in the next commit

---
+++ MowmowRats/src/mowmowrats/Rat.java
@@ -0,0 +1,14 @@
+package mowmowrats;
+
+/**
+ *
+ * @author Anthony Wittenborn
+ */
+public class Rat {
+    //pretty much copy this almost exactly from the tutorial in the discord,
+    //this class replaces the Coins class in the tutorial.
+    //be sure to use javadoc for the code implementation, see the main class and the board class 
+    //for how to do this. Pretty much, if a function has a return type other than void,
+    //specify the return value with @return
+    //if a function accepts a parameter, use @param to document that before each function you define
+}
