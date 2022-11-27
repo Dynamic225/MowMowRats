9f96aeff759550b74a7f968057f60aa84d6607d6
Dynamic <anthonywitherspoon225@gmail.com>
2022-11-27 15:41:19 -0600

    Revert "overhauled title screen and removed runfromstart "
    This reverts commit ebac21bbcb5a3b728e48e7c4fac366b9014b7ab4.

--- MowmowRats/src/mowmowrats/MowmowRats.java
+++ MowmowRats/src/mowmowrats/MowmowRats.java
@@ -1,28 +1,54 @@
 package mowmowrats;
 
 import javax.swing.*;
-import static mowmowrats.Board.COLUMNS;
-import static mowmowrats.Board.ROWS;
-import static mowmowrats.Board.TILE_SIZE;
 
 /**
  * Main application
  *
  * @author Anthony Wittenborn, Matti Lambert,Alexandria Mwaura, Ashley Poteau
  */
-public class MowmowRats {
+public class MowmowRats extends Title implements RunFromStart {
+    /**
+     * Window initialization code
+     */
+    private static void initWindow() {
+        //create window with title "Mowmow and the Rats"
+        JFrame window = new JFrame("MowMow and the Rats");
+        //make the program stop when the window closes
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //create the jpanel and initialize game loop
+        Board board = new Board();
+        //add the board to the window
+        window.add(board);
+        //send key inputs to the board
+        window.addKeyListener(board);
+        
+        //prevent the window from being resizable
+        window.setResizable(false);
+        //fit the window size to the board size
+        window.pack();
+        //open window in center of screen
+        window.setLocationRelativeTo(null);
+        //display the window
+        window.setVisible(true);
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JFrame window = new JFrame("MowMow and the Rats");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(COLUMNS * TILE_SIZE, TILE_SIZE * (ROWS + 2));
-        window.setResizable(false);
-        window.setLocationRelativeTo(null);
-        
-        Title title = new Title();
-        window.add(title);
-        window.setVisible(true);
+        MowmowRats game = new MowmowRats();
+        game.launchTitle();
+    }
+    
+    @Override
+    public void startCode(JFrame title) {
+        title.setVisible(false);
+        //invokeLater() used to prevent our graphics from blocking the GUI
+        //using a lambda expression to replace "new Runnable() {run() {//code}}"
+        SwingUtilities.invokeLater(() -> {
+            initWindow();
+        });
     }
 }
--- MowmowRats/src/mowmowrats/Title.java
+++ MowmowRats/src/mowmowrats/Title.java
@@ -1,99 +1,37 @@
 package mowmowrats;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import static mowmowrats.Board.COLUMNS;
-import static mowmowrats.Board.ROWS;
-import static mowmowrats.Board.TILE_SIZE;
-
 /**
  * Title screen graphics
- * @author Anthony, Ashley
+ * @author ashley
  */
-public class Title extends JPanel {
-    private final int T_WIDTH = TILE_SIZE * COLUMNS;
-    private final int T_HEIGHT = TILE_SIZE * (ROWS + 2);
-    private final int C_WIDTH = TILE_SIZE * 12;
-    private final int C_HEIGHT = TILE_SIZE * 8;
-    private final int B_WIDTH = 140;
-    private final int B_HEIGHT = 65;
-    private final Color B_COLOR = new Color(0xddb439);
-    private final Color T_COLOR = new Color(0x2e0c47);
-    private BufferedImage bgImage;
-    
-    public Title() {
-        setPreferredSize(new Dimension(T_WIDTH, T_HEIGHT));
-        setLayout(null);
-        
-        JPanel container = new JPanel();
-        container.setLayout(null);
-        container.setBounds((T_WIDTH - C_WIDTH)/2, (T_HEIGHT - C_HEIGHT)/2, C_WIDTH, C_HEIGHT);
-        container.setBackground(new Color(0, 0, 0, 0));
-        add(container);
-        
-        JLabel titleText = new JLabel("MowMow and the Rats");
-        titleText.setHorizontalAlignment(JLabel.CENTER);
-        titleText.setFont(new Font("Lato", Font.BOLD, 40));
-        titleText.setForeground(T_COLOR);
-        titleText.setBounds(0, 0, C_WIDTH, 40);
-        container.add(titleText);
-        
-        JButton startButton = new JButton("Start");
-        startButton.setBounds((C_WIDTH - B_WIDTH)/2, TILE_SIZE*2, B_WIDTH, B_HEIGHT);
-        startButton.addActionListener((e) -> runGame());
-        setCommonButtonSettings(startButton);
-        container.add(startButton);
-        
-        JButton quitButton = new JButton("Quit");
-        quitButton.setBounds((C_WIDTH - B_WIDTH)/2, TILE_SIZE*4 + TILE_SIZE/2, B_WIDTH, B_HEIGHT);
-        quitButton.addActionListener((e) -> System.exit(0));
-        setCommonButtonSettings(quitButton);
-        container.add(quitButton);
-    }
-    
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        
-        loadImage();
-        g.drawImage(bgImage, 0, 0, TILE_SIZE * COLUMNS, TILE_SIZE * (ROWS + 2), null);
-        
-        g.setColor(new Color(255, 255, 255, 10));
-        g.fillRect(0, 0, TILE_SIZE * COLUMNS, TILE_SIZE * (ROWS + 2));
-    }
+public abstract class Title implements RunFromStart {
+    private JFrame title;
     
     /**
-     * Load the background image for the class from the file
+     * launches the title screen window
      */
-    private void loadImage() {
-        try {
-           final String imageName = "daybgfr.png";
-            bgImage = ImageIO.read(new File("res/" + imageName));
-        } catch (IOException exc) {
-            System.out.println("Error opening image file: " + exc.getMessage());
-        }
-    }
-    
-    /**
-     * Set the button parameters that are the same
-     * @param button the button to have set
-     */
-    private void setCommonButtonSettings(JButton button) {
-        button.setFocusable(false);
-        button.setFont(new Font("Lato", Font.BOLD, 25));
-        button.setBackground(B_COLOR);
-        button.setForeground(T_COLOR);
-        button.setBorder(BorderFactory.createLineBorder(new Color(0xaf8d3b), 2, true));
-    }
-    
-    /**
-     * code to add the board and start the game
-     */
-    private void runGame() {
+    public void launchTitle(){
+        title = new JFrame();
+        title.setSize(650, 450);
+        title.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        title.setTitle("MowMow and the Rats");
+        title.setVisible(true);
+        title.setLocationRelativeTo(null); //put the title in middle of screen
+        title.setLayout(null);
         
+        // start button, starts game
+        JButton start = new JButton("Start");  
+        start.setSize(100,30);
+        start.setLocation(260,200);  
+        start.addActionListener((event) -> startCode(title));
+        title.add(start);  
+      
+        // quit button, quits application
+        JButton quit = new JButton("Quit");  
+        quit.setSize(100,30);
+        quit.setLocation(260,275); 
+        quit.addActionListener((event) -> System.exit(0));
+        title.add(quit);
     }
 }
