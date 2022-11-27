a16bdc4533deb4e235e30cf81672cde9c31257e8
Anthony <anthonywitherspoon225@gmail.com>
2022-11-27 16:00:06 -0600

    capitalized both 'm's in main class name

---
+++ MowmowRats/src/mowmowrats/MowMowRats.java
@@ -0,0 +1,28 @@
+package mowmowrats;
+
+import javax.swing.*;
+import static mowmowrats.Board.COLUMNS;
+import static mowmowrats.Board.ROWS;
+import static mowmowrats.Board.TILE_SIZE;
+
+/**
+ * Main application
+ *
+ * @author Anthony Wittenborn, Matti Lambert,Alexandria Mwaura, Ashley Poteau
+ */
+public class MowMowRats {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame window = new JFrame("MowMow and the Rats");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(COLUMNS * TILE_SIZE, TILE_SIZE * (ROWS + 2));
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        
+        Title title = new Title();
+        window.add(title);
+        window.setVisible(true);
+    }
+}
